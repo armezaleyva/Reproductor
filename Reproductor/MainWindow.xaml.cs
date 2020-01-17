2ab2c397cc90b6576b8c13c1f2f6505a838c5f63
Armando Meza Leyva <armezaleyva@gmail.com>
2020-01-17 09:44:17 -0700

    Clase 2020-01-17

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -14,20 +14,87 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Microsoft.Win32;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 namespace Reproductor {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        // Lector de archivos
+        AudioFileReader reader;
+        // Exclusivo para salidas
+        WaveOut output;
+
         public MainWindow() {
             InitializeComponent();
+            ListarDispositivosSalida();
+            btnReproducir.IsEnabled = false;
+            btnPausa.IsEnabled = false;
+            btnDetener.IsEnabled = false;
+        }
+
+        void ListarDispositivosSalida() {
+            cbDispositivoSalida.Items.Clear();
+            for (int i = 0; i < WaveOut.DeviceCount; i++) {
+                WaveOutCapabilities capacidades = WaveOut.GetCapabilities(i);
+                cbDispositivoSalida.Items.Add(capacidades.ProductName);
+            }
+            cbDispositivoSalida.SelectedIndex = 0;
         }
 
         private void BtnExaminar_Click(object sender, RoutedEventArgs e) {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true) {
                 txtRutaArchivo.Text = openFileDialog.FileName;
+                btnReproducir.IsEnabled = true;
+            }
+        }
+
+        private void BtnReproducir_Click(object sender, RoutedEventArgs e) {
+            if (output != null && output.PlaybackState == PlaybackState.Paused) {
+                output.Resume();
+                btnReproducir.IsEnabled = false;
+                btnPausa.IsEnabled = true;
+            }
+            else {
+                if (txtRutaArchivo.Text != null && txtRutaArchivo.Text != "") {
+                    reader = new AudioFileReader(txtRutaArchivo.Text);
+                    output = new WaveOut();
+                    output.DeviceNumber = cbDispositivoSalida.SelectedIndex;
+                    output.PlaybackStopped += Output_PlaybackStopped;
+                    output.Init(reader);
+                    output.Play();
+
+                    btnReproducir.IsEnabled = false;
+                    btnPausa.IsEnabled = true;
+                    btnDetener.IsEnabled = true;
+
+                    lblTiempoTotal.Text = reader.TotalTime.ToString().Substring(0, 8);
+                }
+            }
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e) {
+            reader.Dispose();
+            output.Dispose();
+        }
+
+        private void BtnDetener_Click(object sender, RoutedEventArgs e) {
+            if (output != null) {
+                output.Stop();
+                btnReproducir.IsEnabled = true;
+                btnPausa.IsEnabled = false;
+                btnDetener.IsEnabled = false;
+            }
+        }
+
+        private void BtnPausa_Click(object sender, RoutedEventArgs e) {
+            if (output != null) {
+                output.Pause();
+                btnReproducir.IsEnabled = true;
+                btnPausa.IsEnabled = false;
             }
         }
     }
